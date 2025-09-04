94a4621bdf85e9fc87205aa2354d7f58d9f3709c
Michele Memoli <michele.memoli@gmail.com>
2025-09-04 22:16:47 +0100

    feat(app): stuff

--- src/Plop.ts
+++ src/Plop.ts
@@ -1,55 +1,46 @@
-import * as FileSystem from "@effect/platform/FileSystem";
-import * as Path from "@effect/platform/Path";
 import * as Data from "effect/Data";
 import * as Effect from "effect/Effect";
-import nodePlop from "node-plop";
+import * as Lib from "node-plop";
 
 export class PlopApiError extends Data.TaggedError("PlopApiError")<{
   cause: unknown;
 }> {}
 
-export class Plop extends Effect.Service<Plop>()("Plop", {
-  effect: Effect.gen(function* () {
-    const path = yield* Path.Path;
-    const fs = yield* FileSystem.FileSystem;
-
-    const make = Effect.fn("Plop.make")(function* () {
-      const plop = yield* Effect.tryPromise({
-        try: () => nodePlop(),
-        catch: (error) => {
-          return new PlopApiError({
-            cause: error,
-          });
-        },
-      });
-
-      // plop.setGenerator("aggregate", {
-      //   description: "Generate a new DDD module + Effect",
-      //   prompts: [
-      //     {
-      //       type: "input",
-      //       name: "aggregateName",
-      //       message: "Aggregate name:",
-      //     },
-      //     {
-      //       type: "input",
-      //       name: "useCaseName",
-      //       message: "Use case name:",
-      //     },
-      //   ],
-      //   actions: [
-      //     {
-      //       type: "addMany",
-      //       destination: process.cwd(),
-      //       base: path.join(templatesDir, "package"),
-      //       templateFiles: path.join(templatesDir, "package", "**/*"),
-      //     },
-      //   ],
-      // });
-
-      return plop;
+export class PlopApi extends Effect.Service<PlopApi>()("PlopApi", {
+  succeed: Effect.gen(function* () {
+    const plop = yield* Effect.tryPromise({
+      try: () => Lib.default(),
+      catch: (error) => {
+        return new PlopApiError({
+          cause: error,
+        });
+      },
     });
 
-    return { make } as const;
+    return plop;
   }),
 }) {}
+
+export class SetPlopGenerator extends Effect.Service<SetPlopGenerator>()(
+  "SetPlopGenerator",
+  {
+    dependencies: [PlopApi.Default],
+    effect: Effect.gen(function* () {
+      const makePlopApi = yield* PlopApi;
+      const plop = yield* makePlopApi;
+
+      const set = Effect.fn("SetPlopGenerator.set")(function* (
+        name: string,
+        config: Partial<Lib.PlopGeneratorConfig>,
+      ) {
+        yield* Effect.sync(() => plop.setGenerator(name, config));
+
+        return yield* Effect.sync(() => plop.getGenerator(name));
+      });
+
+      return {
+        set,
+      } as const;
+    }),
+  },
+) {}
--- src/bin.ts
+++ src/bin.ts
@@ -3,17 +3,17 @@
 import * as NodeContext from "@effect/platform-node/NodeContext";
 import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
 import * as Effect from "effect/Effect";
-// import * as Logger from "effect/Logger";
-// import * as LogLevel from "effect/LogLevel";
 import * as Layer from "effect/Layer";
+import * as Logger from "effect/Logger";
+import * as LogLevel from "effect/LogLevel";
 import { run } from "./Cli.js";
-import { LocalPlopFile } from "./Plop.js";
+import * as Plop from "./Plop.js";
 
-const appLive = Layer.mergeAll(LocalPlopFile.Default);
+const appLive = Layer.mergeAll(Plop.SetPlopGenerator.Default);
 
 run(process.argv).pipe(
   Effect.provide(appLive),
   Effect.provide(NodeContext.layer),
-  // Logger.withMinimumLogLevel(LogLevel.Debug),
+  Logger.withMinimumLogLevel(LogLevel.Debug),
   NodeRuntime.runMain({ disableErrorReporting: true }),
 );
